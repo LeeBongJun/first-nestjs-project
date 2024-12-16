1ffae52e51b991308e13445bee5ec1dfa87eaa57
bjl0615 <bjl0615@gmail.com>
2024-12-16 21:32:29 +0900

    Database Index Create && Error Handling

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -1,8 +1,12 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({
   name : 'posts',
 })
+
+@Index('IDX_POST_TITLE' , ['title']) // title에 대한 인덱스 이름 지정
+@Index('IDX_POST_AUTHOR_ID' ,  ['authorId']) // title에 대한 인덱스 이름 지정
+@Index('IDX_POST_TITLE_AUTHOR_ID' ,  ['title' , 'authorId'] , {unique : true}) // title에 대한 인덱스 이름 지정
 export class Post {
   @PrimaryGeneratedColumn({
     type : 'int',
